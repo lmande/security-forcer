8154c499f497567513fbc620bf8492aed1adf3fe
ozgur.atom <a.o.altomruk@gmail.com>
2021-02-20 14:43:18 +0300

    remove tests and 2 actions

--- src/Actions/CachedViews.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Lmande\SecurityForcer\Actions;
-
-class CachedViews extends ActionBase
-{
-}
--- src/Actions/DependencyDistributer.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Lmande\SecurityForcer\Actions;
-
-class DependencyDistributer extends ActionBase
-{
-}
