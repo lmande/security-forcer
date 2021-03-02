edc2758012ca3e0f0c593d3d48805c9c6a5b29d8
ozgur.atom <a.o.altomruk@gmail.com>
2021-03-02 09:42:40 +0300

    changed var name for better readability.

--- src/Actions/ActionModifier.php
+++ src/Actions/ActionModifier.php
@@ -9,7 +9,7 @@ abstract class ActionModifier extends Action
 	protected $checkedCodeExistance = false;
 	protected $passDistribution     = false;
 
-	final protected function newClass(string $class, string $regex, string $replacer, string $checkForDuplicate = ''): bool
+	final protected function newClass(string $class, string $regex, string $replacer, string $checkExistanceOf = ''): bool
 	{
 		if (!class_exists($class)) {
 			return false;
@@ -28,7 +28,7 @@ abstract class ActionModifier extends Action
 		if (!$this->checkedCodeExistance) {
 			$contents = File::get($path);
 
-			$this->passDistribution     = $checkForDuplicate && strripos($contents, $checkForDuplicate) !== false;
+			$this->passDistribution     = $checkExistanceOf && strripos($contents, $checkExistanceOf) !== false;
 			$this->checkedCodeExistance = true;
 		}
 
